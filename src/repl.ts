3ed0f211c3ca02f38f253f15de31463748e0d3b0
xDaoud <moshdawd@gmail.com>
2026-02-12 23:51:48 +0200

    feat: Add REPL skeleton

--- src/repl.ts
+++ src/repl.ts
@@ -1,7 +1,36 @@
+import readline from "node:readline";
+
 export function cleanInput(input: string): string[] {
   return input
     .trim()
+    .toLowerCase()
     .split(/\s+/)
     .filter(Boolean);
 }
 
+export interface REPLConfig {
+  input: NodeJS.ReadableStream;
+  output: NodeJS.WritableStream;
+  prompt: string;
+}
+
+export function startREPL(){
+  const config: REPLConfig = {
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > " , 
+  }
+
+  const rl = readline.createInterface(config);
+  rl.prompt();
+
+  rl.on("line", (line: string) => {
+    const words = cleanInput(line);
+    if(words.length === 0){
+      rl.prompt();
+      return;
+    }
+    console.log(`Your command was: ${words[0]}`);
+    rl.prompt();
+  });
+}
