ebaacb134731701992a5b81bdf2c27755da29b8a
xDaoud <moshdawd@gmail.com>
2026-02-14 17:47:04 +0200

    refactor: normalize command return types to Promise<void>

--- src/command_exit.ts
+++ src/command_exit.ts
@@ -1,6 +1,8 @@
 import { State } from "./state.js";
-export function commandExit(state: State): void {
+
+export async function commandExit(state: State): Promise<void> {
     console.log("Closing the Pokedex... Goodbye!");
     state.rl.close();
-    process.exit(0);
+    process.exit(0); // this returns never, so it won't give an error if you removed async
+    //but we put it anyway, for consistency with the other commands
 }
--- src/command_help.ts
+++ src/command_help.ts
@@ -1,5 +1,6 @@
 import { State } from "./state.js";
-export function commandHelp(state: State) {
+
+export async function commandHelp(state: State): Promise<void> {
     console.log("Welcome to the Pokedex!");
     console.log("Usage:\n");
 
